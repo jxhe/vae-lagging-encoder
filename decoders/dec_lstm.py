615853db163587b45f3f3949e7bc99e7d78ff94d
jxhe <junxianh2@gmail.com>
2018-07-20 22:47:23 -0400

    lstm decoder

--- decoders/dec_lstm.py
+++ decoders/dec_lstm.py
@@ -13,7 +13,7 @@ import numpy as np
 
 class LSTMDecoder(object):
     """docstring for LSTMDecoder"""
-    def __init__(self, args):
+    def __init__(self, args, model_init, emb_init):
         super(LSTMDecoder, self).__init__()
         self.ni = args.ni
         self.nh = args.nh
@@ -22,7 +22,8 @@ class LSTMDecoder(object):
         self.emb_initializer = emb_initializer
         self.vocab = vocab
 
-        self.embed = nn.Embedding(len(vocab), args.ni, padding_idx=vocab['<pad>'])
+        # no padding when setting padding_idx to -1
+        self.embed = nn.Embedding(len(vocab), args.ni, padding_idx=-1)
 
         self.dropout_in = nn.Dropout(args.dec_dropout_in)
         self.dropout_out = nn.Dropout(args.dec_dropout_out)
@@ -40,36 +41,32 @@ class LSTMDecoder(object):
         self.pred_linear = nn.Linear(args.nh, len(vocab), bias=False)
 
         vocab_mask = torch.ones(len(vocab))
-        vocab_mask[vocab['<pad>']] = 0
+        # vocab_mask[vocab['<pad>']] = 0
         self.loss = nn.CrossEntropyLoss(weight=vocab_mask, reduce=False)
 
         self.reset_parameters()
 
-    def reset_parameters(self):
-        if self.rnn_initializer is not None:
-            for param in self.parameters():
-                # self.initializer(param)
-                self.rnn_initializer(param)
+    def reset_parameters(self, model_init, emb_init):
+        for param in self.parameters():
+            # self.initializer(param)
+            model_init(param)
 
-        if self.emb_initializer is not None:
-            self.emb_initializer(self.embed.weight.data)
+        emb_init(self.emb.weight)
 
 
-    def decode(self, input_z_sents_len):
+    def decode(self, input, z):
         """
         Args:
             input: (batch_size, seq_len)
             z: (batch_size, n_sample, nz)
-            sents_len: list of sequence lengths
         """
-
-        input, z, sents_len = input_z_sents_len
 
         # not predicting start symbol
         sents_len -= 1
 
         batch_size, n_sample, _ = z.size()
         seq_len = input.size(1)
+
         # (batch_size, seq_len, ni)
         word_embed = self.embed(input)
         word_embed = self.dropout_in(word_embed)
@@ -90,17 +87,12 @@ class LSTMDecoder(object):
         # (batch_size * n_sample, seq_len, ni + nz)
         word_embed = torch.cat((word_embed, z_), -1)
 
-        sents_len = sents_len.unsqueeze(1).expand(batch_size, n_sample).contiguous().view(-1)
-
-        packed_embed = pack_padded_sequence(word_embed, sents_len.tolist(), batch_first=True)
-
         z = z.view(batch_size * n_sample, self.nz)
         # c_init = self.trans_linear(z).unsqueeze(0)
         # h_init = torch.tanh(c_init)
-        h_init = self.trans_linear(z).unsqueeze(0)
-        c_init = Variable(torch.zeros(1, batch_size * n_sample, self.nh).type_as(z.data), requires_grad=False)
-        output, _ = self.lstm(packed_embed, (h_init, c_init))
-        output, _ = pad_packed_sequence(output, batch_first=True)
+        c_init = self.trans_linear(z).unsqueeze(0)
+        h_init = torch.tanh(c_init)
+        output, _ = self.lstm(word_embed, (h_init, c_init))
 
         output = self.dropout_out(output)
 
@@ -109,19 +101,53 @@ class LSTMDecoder(object):
 
         return output_logits
 
-    def reconstruct_error(self, x_sents_len, z):
+    def reconstruct_error(self, x, z):
         """Cross Entropy in the language case
         Args:
-            x_sents_len: (data, sents_len)
             x: (batch_size, seq_len)
             z: (batch_size, n_sample, nz)
         Returns:
-            loss: sum of loss over minibatch
+            loss: (batch_size, n_sample). Loss 
+            across different sentence and z
         """
 
-        pass
+        #remove end symbol
+        src = x[:, :-1]
 
-    def log_probability(self, x_sents_len, z):
-        pass
+        # remove start symbol
+        tgt = x[:, 1:]
+
+        batch_size, seq_len = src.size()
+        n_sample = z.size(1)
+
+        # (batch_size * n_sample, seq_len, vocab_size)
+        output_logits = self.decode(src, z)
+
+        if n_sample == 1:
+            tgt = tgt.contiguous().view(-1)
+        else:
+            # (batch_size * n_sample * seq_len)
+            tgt = tgt.unsqueeze(1).expand(batch_size, n_sample, seq_len) \
+                     .contiguous().view(-1)
+
+        # (batch_size * n_sample * seq_len)
+        loss = self.loss(output_logits.view(-1, output_logits.size(2)),
+                         tgt)
+
+
+        # (batch_size, n_sample)
+        return loss.view(batch_size, n_sample, -1).sum(-1)
+
+    def log_probability(self, x, z):
+        """Cross Entropy in the language case
+        Args:
+            x: (batch_size, seq_len)
+            z: (batch_size, n_sample, nz)
+        Returns:
+            log_p: (batch_size, n_sample). 
+            log_p(x|z) across different x and z
+        """
+
+        return -self.reconstruct_error(x, z)
         
 
