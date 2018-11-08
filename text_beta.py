unbias correction for au correct the variance estimator to be an unbiased one

text_beta.py
@@ -176,7 +176,7 @@ def calc_au(model, test_data_batch, delta=0.01):
     au_var = means - au_mean
     ns = au_var.size(0)
 
-    au_var = (au_var ** 2).sum(dim=0) / ns
+    au_var = (au_var ** 2).sum(dim=0) / (ns - 1)
 
     return (au_var >= delta).sum().item()
 